f4c30ee2f7ba233a6c772fb580cd4d1ab9595e48
Evan Huus <evan@opslevel.com>
2026-05-06 11:00:16 -0400

    Fix lint issue

--- config/config.go
+++ config/config.go
@@ -4,40 +4,38 @@ import (
 	"opslevel-agent/controller"
 )
 
-var (
-	DefaultConfiguration = &Configuration{
-		Selectors: []controller.Selector{
-			{
-				ApiVersion: "v1",
-				Kind:       "Namespace",
-			},
-			{
-				ApiVersion: "apps/v1",
-				Kind:       "Deployment",
-			},
-			{
-				ApiVersion: "apps/v1",
-				Kind:       "StatefulSet",
-			},
-			{
-				ApiVersion: "apps/v1",
-				Kind:       "DaemonSet",
-			},
-			{
-				ApiVersion: "networking.k8s.io/v1",
-				Kind:       "Ingress",
-			},
-			{
-				ApiVersion: "v1",
-				Kind:       "Service",
-			},
-			{
-				ApiVersion: "batch/v1",
-				Kind:       "CronJob",
-			},
+var DefaultConfiguration = &Configuration{
+	Selectors: []controller.Selector{
+		{
+			ApiVersion: "v1",
+			Kind:       "Namespace",
 		},
-	}
-)
+		{
+			ApiVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		{
+			ApiVersion: "apps/v1",
+			Kind:       "StatefulSet",
+		},
+		{
+			ApiVersion: "apps/v1",
+			Kind:       "DaemonSet",
+		},
+		{
+			ApiVersion: "networking.k8s.io/v1",
+			Kind:       "Ingress",
+		},
+		{
+			ApiVersion: "v1",
+			Kind:       "Service",
+		},
+		{
+			ApiVersion: "batch/v1",
+			Kind:       "CronJob",
+		},
+	},
+}
 
 type Configuration struct {
 	Selectors []controller.Selector `json:"selectors" yaml:"selectors"`
